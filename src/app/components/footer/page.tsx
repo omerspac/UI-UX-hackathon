3749991db599a4794934e2b482b053ff82caba68
omerspac <omerspac@gmail.com>
2024-12-08 19:06:48 +0500

    c2

--- src/app/components/footer/page.tsx
+++ src/app/components/footer/page.tsx
@@ -1,3 +1,5 @@
+import css from "./page.module.css"
+
 export default function Footer() {
     return(
         <></>
--- src/app/components/header/page.tsx
+++ src/app/components/header/page.tsx
@@ -1,5 +1,12 @@
+import css from "./page.module.css";
+import Image from "next/image";
+
 export default function Header() {
-    return(
-        <></>
-    )
+    return (
+        <div className={css.header}>
+            <div className={css.wrapper}>
+                <p className={css.logo}>MORENT</p>
+            </div>
+        </div>
+    );
 }
